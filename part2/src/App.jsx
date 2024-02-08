c147c9c27535c2f8ae59041c3d558a44f008fc2a
Toni Hynynen <toni.hynynen@hotmail.com>
2024-02-08 20:49:28 +0200

    2.9

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -13,10 +13,16 @@ const ListItem = ({items}) =>{
   )
 }
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '044-1234567' }]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  
+  const [filterName, setFilterName] = useState('')
+
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
@@ -27,17 +33,18 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
-
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
   const handlePhoneChange = (event) => {
     setNewNumber(event.target.value)
   }
-
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={event =>setFilterName(event.target.value)} value={filterName}/>
+      </div>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newNumber} onChange={handlePhoneChange}/></div>
@@ -46,9 +53,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        <PersonList list={persons}/>
+        <PersonList list={persons.filter(person => person.name.includes(filterName))}/>
     </div>
   )
-
 }
 export default App 

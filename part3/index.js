f1e1340a6bb41b92d0f884f3d38e61f221f0ef0b
Toni Hynynen <toni.hynynen@hotmail.com>
2024-02-25 14:35:20 +0200

    dist upgrade

--- part3/index.js
+++ part3/index.js
@@ -35,6 +35,7 @@ const requestLogger = (request, response, next) => {
 }
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 app.use(requestLogger)
 morgan.token("body",(request) => JSON.stringify(request.body))
 app.use(morgan("tiny"))
